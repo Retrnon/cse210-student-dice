fca4fc879068546b21293bacf4ac6ec1259d892c
Jadan White <34385383+Retrnon@users.noreply.github.com>
2021-10-05 15:58:20 -0600

    Made changes
    Made changes to thrower.py and README.md

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -1,3 +1,30 @@
 import random
 
-# TODO: Define the Thrower class here.
+class Thrower(object):
+    
+    def __init__(self):
+        """
+        Initializes object attributes
+        """
+        self.dice = []
+        self.score = 0
+
+    def throw_dice(self):
+        """
+        Clears previous Dice and throws Dice and randomizes 5d6 dice in self.dice
+        """
+        self.dice.clear()
+        for x in range(5):
+            self.dice.append(random.randint(1, 6))
+
+    def get_points(self):
+        """
+        Counts instances of 5s and 1s and returns the sum
+        """
+        return (self.dice.count(5) * 50 + self.dice.count(1) * 100)
+
+    def can_throw(self):
+        """
+        Checks and returns True if there are any instances of 1s ir 5s in self.dice
+        """
+        return any([die == 1 for die in self.dice]) or any([die == 5 for die in self.dice])
